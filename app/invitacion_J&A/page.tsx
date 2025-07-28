980e7079bdc70f30644ea5b289a88f975e8dbc30
agustin-devfs <agustin.devfs@gmail.com>
2025-07-28 15:58:24 -0300

    invitacion

---
+++ app/invitacion_J&A/page.tsx
@@ -0,0 +1,9 @@
+export default function Invitacion() {
+  return (
+    <iframe
+      src="https://v0-josue-and-ariana-wedding.vercel.app"
+      className="w-full h-screen border-0"
+      allow="autoplay"
+    />
+  )
+}
