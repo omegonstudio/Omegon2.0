2b464d83e7d375b9ac9d7f54e4f46561a4008e12
agustin-devfs <agustin.devfs@gmail.com>
2025-08-19 16:23:44 -0300

    XVluana

---
+++ app/invitacion_15_luana/page.tsx
@@ -0,0 +1,9 @@
+export default function Invitacion() {
+  return (
+    <iframe
+      src="https://v0-tarjeta-15-anos.vercel.app/"
+      className="w-full h-screen border-0"
+      allow="autoplay"
+    />
+  )
+}
