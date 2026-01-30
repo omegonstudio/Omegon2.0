4b8728aa0a1fa320a47d40ad406785cb41955aac
omegonstudio <omegon.info@gmail.com>
2026-01-30 12:31:56 -0300

    cambio de emailjs + delete tarjetas

--- app/invitacion_15_luana/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Invitacion() {
-  return (
-    <iframe
-      src="https://v0-tarjeta-15-anos.vercel.app/"
-      className="w-full h-screen border-0"
-      allow="autoplay"
-    />
-  )
-}
--- app/invitacion_J&A/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Invitacion() {
-  return (
-    <iframe
-      src="https://v0-josue-and-ariana-wedding.vercel.app"
-      className="w-full h-screen border-0"
-      allow="autoplay"
-    />
-  )
-}
